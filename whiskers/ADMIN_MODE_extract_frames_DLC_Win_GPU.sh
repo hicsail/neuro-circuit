e182410cdb2e0eda60a2b578f3f1101db76860ae
Zackhardtoname <zack.hardtoname@gmail.com>
2019-06-07 07:07:19 -0400

    DLC: add Mac script

--- whiskers/ADMIN_MODE_extract_frames_DLC_Win_GPU.sh
+++ whiskers/ADMIN_MODE_extract_frames_DLC_Win_GPU.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# move to /c/Users/YOUR_USERNAME/Documents/GitHub/neuro-circuit/bash_scripts
 echo "activating environment"
 conda activate dlc-windowsGPU
 python run.py
--- whiskers/extract_frames_DLC_Mac.sh
+++ whiskers/extract_frames_DLC_Mac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # move to /c/Users/YOUR_USERNAME/Documents/GitHub/neuro-circuit/bash_scripts
 echo "activating environment"
-conda activate dlc-windowsGPU
+source activate test-dlc-macOS-CPU
 python run.py
 read -p "all set: press enter to continue"
