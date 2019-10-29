11eacb05a3c9c4c10eaa2e81be93d41e9edd4bc0
Abed Ghanbari <abedxghanbari@gmail.com>
2019-10-29 17:06:44 -0400

    more flexible terastitcher file formatter

--- preprocessing/stitch_tiles.py
+++ preprocessing/stitch_tiles.py
@@ -3,8 +3,6 @@ import os
 from shutil import copyfile
 from terastitcher_wrapper import TeraStitcherWrapper
 
-TILE_NAMES = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']
-COORDINATES = ['000010', '003150', '006300']
 TERA_FOLDER = 'tera_tiles'
 STITCHED_FOLDER = '02_Stitched'
 TERA_EXE_DIR = "Z:/Personnel/Abed/Software/buildtera2/bin/Release/terastitcher.exe"
@@ -15,17 +13,37 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--input_file", "-f", help="file format of the tiles")
 	parser.add_argument("--input_dir", "-d", help="directory of the tiles")
+	parser.add_argument("--grid_size", "-g", help="2 digit number representing grid dimension. e.g. 43 --> 4x3 grid")
+	parser.add_argument("--spacing", "-s", help="assuming equal space in x/y directions, how much we move between tiles")
+
 
 	args = parser.parse_args()
 	file_format = args.input_file
 	input_dir = args.input_dir
+	s = int(args.spacing)
+
+	os.chdir(input_dir)
+	filenames = [file for file in glob.glob("*.tif")]
+	alphabet = list('ABCDEFGHIJK')
+	tilenames = []
+	for filename in filenames:
+	    for i in range(len(alphabet)):
+	        for j in range(10):
+	            if filename.find(alphabet[i]+str(j+1)) != -1:
+	                tilenames.append(alphabet[i]+str(j+1).upper())
+
+	assert grid_size[0]*grid_size[1] == len(tilenames), "tilenames and grid_size don't match"
+
+	grid_size = [int(args.grid_size[0]), int(args.grid_size[1])] 
+
+	coordinates = [str(i*s*10).zfill(6) for i in range(max(grid_size))]
 
 	#check that we have access to Z drive
 	if not os.path.exists(TERA_EXE_DIR):
 		raise FileNotFoundError('Cannot find TeraStitcher. Is the Z drive accessible?')
 
 	#check that input directory exists
-	assert os.path.isdir(input_dir)
+	assert os.path.isdir(input_dir), "input directory doesn't exist!"
 
 	#check that file format is correct
 	if file_format.find('##') == -1:
@@ -48,13 +66,13 @@ if __name__ == "__main__":
 	os.makedirs(tera_dir)
 	assert os.path.exists(tera_dir)
 
-	for i in range(3): # row
-	    os.mkdir(tera_dir+'\\'+COORDINATES[i])
-	    for j in range(3): # col
-	        dst_tile = tera_dir+'\\'+COORDINATES[i]+'\\'+COORDINATES[i]+'_'+COORDINATES[j]+'\\'
+	for i in range(grid_size[0]): # row
+	    os.mkdir(tera_dir+'\\'+coordinates[i])
+	    for j in range(grid_size[1]): # col
+	        dst_tile = tera_dir+'\\'+coordinates[i]+'\\'+coordinates[i]+'_'+coordinates[j]+'\\'
 	        os.mkdir(dst_tile)
-	        copyfile(input_dir + '\\' + filelist[j+i*3], dst_tile + '\\' +COORDINATES[0]+'.tif')
-	        print( filelist[j+i*3] + ' ---> ' + dst_tile)
+	        copyfile(input_dir + '\\' + filelist[j+i*grid_size[0]], dst_tile + '\\' +coordinates[0]+'.tif')
+	        print( filelist[j+i*grid_size[0]] + ' ---> ' + dst_tile)
 
 
 	# Stitch tiles
